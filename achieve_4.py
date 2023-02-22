9b49363a1a5f2c055f968fd7942dfa22928d7f34
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-22 16:45:23 -0500

    Good

--- achieve_4.py
+++ achieve_4.py
@@ -1,32 +1,33 @@
 def celToFah (cel) :
     conv = (cel * 1.8) + 32
     return conv
-
-
-
 def fahToCel (fah) :
     conv = (5/9) * (fah-32)
     return conv
-
-
 def mphToKm (mph) :
     conv = (mph * 1.609344)
     return conv
-
 def kmToMph (km) :
     conv = (km / 1.609344)
     return conv
-
 choice = input("Welcome to the conversion calculator! Would you like to convert: \n1. Temperature \n2. Speed\n").strip()
-if choice == 1:
+if choice == "1":
     choiceT = input("Would you like to convert from Celcicus (C, c) or Fahernite (F, f):\n").strip().lower()
     if choiceT == "c" :
-        celinput = celToFah(float(input("Enter your celcius temperature:")))
+        celinput = celToFah(float(input("Enter your celcius temperature:"))).strip()
+        print("The temperature is {} fahernite".format(celinput))    
     elif choiceT == "f" :
-        fahinput = fahToCel(float(input("Enter your fahernite:")))
+        fahinput = fahToCel(float(input("Enter your fahernite:"))).strip()
+        print("The temperature is {} degrees celcius".format(fahinput))
     else:
         print("Incorrect input!")
-elif choice == 2:
-    choiceT = input("Would you like to convert from Kilometers Per Hour (KMH, kmh) or Miles Per Hour (MPH, mph):\n")
+elif choice == "2":
+    choiceS = input("Would you like to convert from Kilometers Per Hour (KMH, kmh) or Miles Per Hour (MPH, mph):\n").strip().lower()
+    if choiceS == "kmh" :
+        kmhInput = kmToMph(float(input("Enter your speed in kilometers per hour:\n"))).strip()
+        print("The speed is {} miles per hour".format(kmhInput))
+    elif choiceS == "mph" :
+        mphInput = mphToKm(float(input("Enter your speed in miles per hour:\n"))).strip()
+        print("The speed is {} kilometers per hour".format(mphInput))             
 else:
     print("Incorrect entry")    
