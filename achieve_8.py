17887038380cb1c438ccff38dd2919e2028aa664
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-03-22 14:58:04 -0400

    ok

--- achieve_8.py
+++ achieve_8.py
@@ -6,15 +6,19 @@
 #
 #This program will output the requested conversion.
 def celToFah (cel) : #celsius to fahrenheit function
+    
     conv = (cel * 1.8) + 32
     return conv
 def fahToCel (fah) : #fahrenheit to celsius function
+    
     conv = (5/9) * (fah-32)
     return conv
 def mphToKm (mph) : #Miles to kilometers function
+    
     conv = (mph * 1.609344)
     return conv
 def kmToMph (km) : #Kilometers to miles function
+   
     conv = (km / 1.609344)
     return conv
 
@@ -27,20 +31,31 @@ while True:
         print("Invalid value")
     else:
         if choice == 1: #Temperature conversion selection
-            choiceT = input("Would you like to convert from Celcicus (C, c) or Fahernite (F, f):\n").strip().lower() #Prompting user input and stripping spaces, converting to lower case and saving it to a variable
-            if choiceT == "c" :
-                celinput = celToFah(float(input("Enter your celcius temperature:"))) #Prompting user input casting float on the input and saving to variable
-                print("The temperature is {} fahernite".format(celinput))    
-            elif choiceT == "f" :
-                fahinput = fahToCel(float(input("Enter your fahernite:"))) #Prompting user input casting float on the input and saving to variable
-                print("The temperature is {} degrees celcius".format(fahinput))
-            else:
-                print("Incorrect input!")
+            while True:
+                try:
+                    choiceT = input("Would you like to convert from Celcicus (C, c) or Fahernite (F, f):\n").strip().lower() #Prompting user input and stripping spaces, converting to lower case and saving it to a variable
+                except ValueError:
+                    print("Invalid value")
+                else:
+                    if choiceT == "c" :
+                        while True:
+                            try:
+                                celinput = celToFah(float(input("Enter your celcius temperature:"))) #Prompting user input casting float on the input and saving to variable
+                                print("The temperature is {} fahernite".format(celinput))  
+                                break
+                            except ValueError:    
+                                print("Integers and decimals only")
+                              
+                    elif choiceT == "f" :
+                        fahinput = fahToCel(float(input("Enter your fahernite:"))) #Prompting user input casting float on the input and saving to variable
+                        print("The temperature is {} degrees celcius".format(fahinput))
+                
         elif choice == 2:
-            choiceS = input("Would you like to convert from Kilometers Per Hour (KMH, kmh) or Miles Per Hour (MPH, mph):\n").strip().lower()  #Prompting user input and stripping spaces, converting to lower case and saving it to a variable
-            if choiceS == "kmh" :
-                kmhInput = kmToMph(float(input("Enter your speed in kilometers per hour:\n"))) #Prompting user input casting float on the input and saving to variable
-                print("The speed is {} miles per hour".format(kmhInput))
-            elif choiceS == "mph" :
-                mphInput = mphToKm(float(input("Enter your speed in miles per hour:\n"))) #Prompting user input casting float on the input and saving to variable
-                print("The speed is {} kilometers per hour".format(mphInput))             
+            while True:
+                choiceS = input("Would you like to convert from Kilometers Per Hour (KMH, kmh) or Miles Per Hour (MPH, mph):\n").strip().lower()  #Prompting user input and stripping spaces, converting to lower case and saving it to a variable
+                if choiceS == "kmh" :
+                    kmhInput = kmToMph(float(input("Enter your speed in kilometers per hour:\n"))) #Prompting user input casting float on the input and saving to variable
+                    print("The speed is {} miles per hour".format(kmhInput))
+                elif choiceS == "mph" :
+                    mphInput = mphToKm(float(input("Enter your speed in miles per hour:\n"))) #Prompting user input casting float on the input and saving to variable
+                    print("The speed is {} kilometers per hour".format(mphInput))             
